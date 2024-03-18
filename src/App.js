fc308dee9bcad46addda7e4867f04bc381483117
achay007 <akshayachz09@gmail.com>
2024-03-18 21:27:08 +0530

    blog navbar , data loaded , routing 1

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import './App.css';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 import ArticleListPage from './pages/ArticleListPage';
 import ArticlePage from './pages/ArticlePage';
-
+import NavBar from './NavBar';
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
-     <h1>
-      My Awsome Blog
-     </h1>
+     <NavBar/>
      <div id="page-body">
-      Welcome to my blog!
+        <Routes>
+          <Route path="/" element={<HomePage/>}/>
+          <Route path="/about" element={<AboutPage/>}/>
+          <Route path="/articles" element={<ArticleListPage/>}/>
+          <Route path="/articles/:articleId" element={<ArticlePage/>}/>
+        </Routes>
      </div>
     </div>
+    </BrowserRouter>
   );
 }
 
--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,8 +1,20 @@
-import React from 'react'
+import React from "react";
+import { useParams } from "react-router-dom";
+import articles from "./article-content";
 
 export const ArticlePage = () => {
+  const params = useParams();
+  const { articleId } = params;
+  const article = articles.find((article) => article.name === articleId);
+
   return (
-    <h1>This is the ArticlePage</h1>
-  )
-}
+    <>
+      <h1>{article.title}</h1>
+      {article.content.map((paragraph) => (
+        <p>{paragraph}</p>
+      )
+      )}
+    </>
+  );
+};
 export default ArticlePage;
